465a3f7468f63bfb252f405dcc82f1585f010e87
CyntiaDalesio <cyntiam.135@gmail.com>
2022-04-05 18:03:01 -0400

    obtención de array con los link

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-
-const functions = require('./functions.js');
 const readline = require('readline');
+const colors = require('colors/safe');
+const functions = require('./functions.js');
 
-let answer = '';
-let arrayMD = [];
-
-let interfazCaptura = readline.createInterface({
+let array1 = [];
+const urlOk = [];
+const urlNotOk = [];
+const interfazCaptura = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-interfazCaptura.question('Ingrese la ruta: ', function (respuesta) {
+interfazCaptura.question('Ingrese la ruta: ', (respuesta) => {
+  let answer = '';
+  let arrayMD = [];
   answer = `${respuesta}`;
-  console.log(`La ruta ingresada es: ${answer}`);
-  if (answer != '') {
+  // console.log(`La ruta ingresada es: ${answer}`);
+  process.stdout.write(colors.blue(`La ruta ingresada es: ${answer}\n`));
+  if (answer !== '') {
     functions.existPath(answer).then(() => {
       if (!functions.pathAbsolute(answer)) {
         console.log('la ruta ingresada es RELATIVA. . . se transformatá en absoluta');
@@ -22,28 +25,26 @@ interfazCaptura.question('Ingrese la ruta: ', function (respuesta) {
       }
       return answer;
     })
-    .then((answer) => {
+      .then(() => {
+        if (!functions.isFile(answer)) throw new TypeError(console.log('No puede ingresar una carpeta'));
 
-      if (!functions.isFile(answer)) throw new TypeError(console.log('No puede ingresar una carpeta'));
+        console.log('lo ingresado es un archivo');
+        if (functions.verifyExtensionMD(answer)) {
+          arrayMD = functions.read(answer);
+        }
 
-      console.log('lo ingresado es un archivo');
-      if (functions.verifyExtensionMD(answer)) {
-        arrayMD = functions.read(answer);
-      }
- 
-      return arrayMD;
-    })
-      .then((arrayMD) => {
-        console.log('El arrayMD del then es:', arrayMD);
-
-      }).catch((err) => {
-        console.log('La ruta  no existe');
-          console.log(err);
+        return arrayMD;
       })
-  }else{
+      .then((array2) => {
+        console.log('El array1 tiene que el valor:', array2);
+      })
+      .catch((err) => {
+        console.log('La ruta  no existe');
+        console.log(err);
+      });
+  } else {
     console.log('No ha ingresado ninguna ruta');
   }
 
-
   interfazCaptura.close();
-})
+});
