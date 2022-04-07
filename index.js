0d7d86b8b7a01f718bd104af974dad8a7109b85d
CyntiaDalesio <cyntiam.135@gmail.com>
2022-04-06 20:38:39 -0400

    args con validate y stats

--- index.js
+++ index.js
@@ -15,20 +15,16 @@ interfazCaptura.question('Ingrese la ruta: ', (respuesta) => {
   let answer = '';
   let arrayMD = [];
   answer = `${respuesta}`;
-  // process.stdout.write(colors.blue(`La ruta ingresada es: ${answer}\n`));
   if (answer !== '') {
     functions.existPath(answer).then(() => {
       if (!functions.pathAbsolute(answer)) {
         console.log('la ruta ingresada es RELATIVA. . . se transformará en absoluta');
         answer = functions.pathTransformationAbsolute(answer);
-        console.log('absoluta: ', answer);
       }
       return answer;
     })
       .then(() => {
         if (!functions.isFile(answer)) throw new TypeError(console.log('No puede ingresar una carpeta'));
-
-        // console.log('lo ingresado es un archivo');
         if (functions.verifyExtensionMD(answer)) {
           arrayMD = functions.read(answer);
         }
@@ -36,7 +32,6 @@ interfazCaptura.question('Ingrese la ruta: ', (respuesta) => {
         return arrayMD;
       })
       .then((arrayLink) => {
-        // console.log('El array1 tiene que el valor:', arrayLink);
         const promiseArr = arrayLink.map((url) => functions.verifityLink(url).then((status) => {
           arrayJson.push(status);
         })
@@ -47,36 +42,53 @@ interfazCaptura.question('Ingrese la ruta: ', (respuesta) => {
         return Promise.all(promiseArr);
       })
       .then(() => {
-        // console.log('argumentos', args[2]);
         let countValid = 0;
         let countInvalid = 0;
+        if (args[3] === '--validate' && args[2] === '--stats') {
+          arrayJson.forEach((e) => {
+            if (e.status) {
+              countValid += 1;
+            } else {
+              countInvalid += 1;
+            }
+          });
+          console.log(colors.yellow('link encontrados:', arrayJson.length));
+          console.log(colors.green('link validos:', countValid));
+          console.log(colors.red('link rotos:', countInvalid));
+          arrayJson.forEach((e) => {
+            if (e.status) {
+              console.log(colors.green(`Link: ${e.linkname} Status: ${e.status}`));
+            } else {
+              console.log(colors.red(`Link: ${e.linkname} Status: ${e.status}`));
+            }
+          });
+        } else {
+          switch (args[2]) {
+            case '--stats':
+              console.log(colors.yellow('link encontrados:', arrayJson.length));
+              arrayJson.forEach((e) => {
+                if (e.status) {
+                  countValid += 1;
+                } else {
+                  countInvalid += 1;
+                }
+              });
+              console.log(colors.green('link validos:', countValid));
+              console.log(colors.red('link rotos:', countInvalid));
 
-        switch (args[2]) {
-          case '--stats':
-            console.log('link encontrados:', arrayJson.length);
-            arrayJson.forEach((e) => {
-              if (e.status) {
-                countValid += 1;
-              } else {
-                countInvalid += 1;
-              }
-            });
-            console.log(colors.green('link validos:', countValid));
-            console.log(colors.red('link rotos:', countInvalid));
-
-            break;
-          case '--validate':
-            // console.log('El final es:', arrayJson);
-            arrayJson.forEach((e) => {
-              if (e.status) {
-                console.log(colors.green(`Link: ${e.linkname} Status: ${e.status}`));
-              } else {
-                console.log(colors.red(`Link: ${e.linkname} Status: ${e.status}`));
-              }
-            });
-            break;
-          default:
-            break;
+              break;
+            case '--validate':
+              arrayJson.forEach((e) => {
+                if (e.status) {
+                  console.log(colors.green(`Link: ${e.linkname} Status: ${e.status}`));
+                } else {
+                  console.log(colors.red(`Link: ${e.linkname} Status: ${e.status}`));
+                }
+              });
+              break;
+            default:
+              break;
+          }
         }
       });
   } else {
